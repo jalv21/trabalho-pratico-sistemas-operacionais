fc25a41418c6a85fad716200f1e3e0400f71271f
Jalv <jalv.rodrigues10@gmail.com>
2025-12-12 12:58:24 -0300

    feat(MemoriaFisica.java): Recriando classe MemoriaFisica atualizada representando a memória física contendo os frames

--- src/MemoriaFisica.java
+++ src/MemoriaFisica.java
@@ -1,3 +1,22 @@
 public class MemoriaFisica {
+    private Frame[] frames;
+    private Configuracao config;
+    private int proximoFrameDisponivel;
 
+    public MemoriaFisica(Configuracao config) {
+        // TODO
+    }
+
+    public Frame encontrarFrameDisponivel() {
+        // TODO
+        return null;
+    }
+
+    public void carregarPagina(Pagina pag, Frame frame, EntradaPagina entrada) {
+        // TODO
+    }
+
+    public boolean cheia() {
+        return (frames.length == config.quantFrames());
+    }
 }
