354918a8ebbc595647fe1770ca90afa0cfe957b0
Jalv <jalv.rodrigues10@gmail.com>
2025-12-12 08:08:20 -0300

    revert: removendo código antigo da classe MemoriaFisica

--- src/MemoriaFisica.java
+++ src/MemoriaFisica.java
@@ -1,17 +1,3 @@
-public class MemoriaFisica<E> extends Memoria<E>{
+public class MemoriaFisica {
 
-    public MemoriaFisica(int tamanhoBytes, EArquitetura arquitetura) {
-        super(tamanhoBytes, arquitetura);
-    }
-
-    @Override
-    protected void alocar(int endereco, E valor) {
-        // TODO Implementação específica para alocar na memória física
-    }
-
-    @Override
-    protected E remover(int endereco, E valor) {
-        // TODO Implementação específica para remover da memória física
-        return null;
-    }
 }
