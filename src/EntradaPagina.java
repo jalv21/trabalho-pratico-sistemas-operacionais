2d45e8e549a4d0eacc3e73930a977c557ffb0951
Jalv <jalv.rodrigues10@gmail.com>
2025-12-12 09:27:40 -0300

    feat: criando classse EntradaPagina representando uma entrada na tabela de páginas do processo

---
+++ src/EntradaPagina.java
@@ -0,0 +1,28 @@
+public class EntradaPagina {
+    private int numFrame;
+    private boolean presente;
+    private boolean modificada;
+    private boolean referenciada;
+
+    public EntradaPagina() {
+        numFrame = 0;
+        presente = modificada = referenciada = false;
+    }
+
+    public boolean isPresente() {
+        return presente;
+    }
+
+    public boolean isModificada() {
+        return modificada;
+    }
+
+    public boolean isReferenciada() {
+        return referenciada;
+    }
+
+    public boolean estaNoSwap() {
+        // TODO Método para verificar se uma entrada está no Swap
+        throw new UnsupportedOperationException("Método ainda não implementado.");
+    }
+}
