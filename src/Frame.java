10dd580ba1ec0daeac13ff69ede2839d159ef241
Jalv <jalv.rodrigues10@gmail.com>
2025-12-12 09:08:37 -0300

    feat: criando classe Frame representando um frame(quadro) na memória física

---
+++ src/Frame.java
@@ -0,0 +1,19 @@
+public class Frame {
+    private int numFrame;
+    private Pagina pagina;
+    private long tempoCarregado;
+
+    public Frame(int numFrame, Pagina pagina) {
+        this.numFrame = numFrame;
+        this.pagina = pagina;
+        atualizarTempoAcesso();
+    }
+
+    public boolean vazio() {
+        return (this.pagina == null);
+    }
+
+    private void atualizarTempoAcesso() {
+        this.tempoCarregado = 0;
+    }
+}
